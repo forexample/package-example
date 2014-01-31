f51bdb247af79580fa80bbd30a573f3531aaf2b1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-31 21:24:09 +0400

    Add Boo noconfig build

--- build.sh
+++ build.sh
@@ -5,18 +5,22 @@ rm -rf _install
 
 CMAKE=cmake
 
-echo "Foo Release"
+echo "&&&&& Foo Release"
 ${CMAKE} -HFoo -B_builds/Foo-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="`pwd`/_install"
 ${CMAKE} --build _builds/Foo-release --target install
 
-echo "Foo Debug"
+echo "&&&&& Foo Debug"
 ${CMAKE} -HFoo -B_builds/Foo-debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="`pwd`/_install"
 ${CMAKE} --build _builds/Foo-debug --target install
 
-echo "Boo Release"
+echo "&&&&& Boo Release"
 ${CMAKE} -HBoo -B_builds/Boo-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
 ${CMAKE} --build _builds/Boo-release
 
-echo "Boo Debug"
+echo "&&&&& Boo Debug"
 ${CMAKE} -HBoo -B_builds/Boo-debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
 ${CMAKE} --build _builds/Boo-debug
+
+echo "&&&&& Boo no build type"
+${CMAKE} -HBoo -B_builds/Boo-noconfig -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
+${CMAKE} --build _builds/Boo-noconfig

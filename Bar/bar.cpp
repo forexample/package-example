1c646b1d35c4b68a324356d8b32d2b45ddc09eb8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-08-20 20:30:32 +0400

    Add target bar (executable)

---
+++ Bar/bar.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <boo.hpp>
+
+int main() {
+  std::cout << "boo said: " << boo() << std::endl;
+}
--- build.sh
+++ build.sh
@@ -15,12 +15,16 @@ ${CMAKE} --build _builds/Foo-debug --target install
 
 echo "&&&&& Boo Release"
 ${CMAKE} -HBoo -B_builds/Boo-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
-${CMAKE} --build _builds/Boo-release
+${CMAKE} --build _builds/Boo-release --target install
 
 echo "&&&&& Boo Debug"
 ${CMAKE} -HBoo -B_builds/Boo-debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
-${CMAKE} --build _builds/Boo-debug
+${CMAKE} --build _builds/Boo-debug --target install
 
 echo "&&&&& Boo no build type"
 ${CMAKE} -HBoo -B_builds/Boo-noconfig -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
 ${CMAKE} --build _builds/Boo-noconfig
+
+echo "&&&&& Bar Release"
+${CMAKE} -HBar -B_builds/Bar-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_VERBOSE_MAKEFILE=ON
+${CMAKE} --build _builds/Bar-release

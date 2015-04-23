444b867f96db8b4f1692aa2b17c7f67b5c54ea35
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-23 15:08:02 +0200

    Remove no-config build type

--- jenkins.py
+++ jenkins.py
@@ -29,38 +29,34 @@ else:
   do_call(['which', 'cmake'])
 
 do_call(['cmake', '--version'])
+install_dir = os.path.join('_install')
 
 def run_build(projname, buildtype, install, verbose):
-  if buildtype:
-    dir_tag = buildtype
-  else:
-    dir_tag = 'noconfig'
   print('-' * 80)
-  print("+ {} {}".format(projname, dir_tag))
+  print("+ {} {}".format(projname, buildtype))
   print('-' * 80)
-  build_dir = os.path.join('_builds', '{}-{}'.format(projname, dir_tag))
-  install_dir = os.path.join('_install')
+  build_dir = os.path.join('_builds', '{}-{}'.format(projname, buildtype))
   args = [
       'cmake',
       '-H{}'.format(projname),
       '-B{}'.format(build_dir),
+      '-DCMAKE_BUILD_TYPE={}'.format(buildtype),
       '-DCMAKE_INSTALL_PREFIX={}'.format(install_dir)
   ]
-  if buildtype:
-    args += ['-DCMAKE_BUILD_TYPE={}'.format(buildtype)]
+  if buildtype != 'Release':
+    args += ['-DCMAKE_{}_POSTFIX=-{}'.format(buildtype.upper(), buildtype)]
   if verbose:
     args += ['-DCMAKE_VERBOSE_MAKEFILE=ON']
   do_call(args)
 
-  args = ['cmake', '--build', build_dir]
-  if buildtype:
-    args += ['--config', buildtype]
+  args = ['cmake', '--build', build_dir, '--config', buildtype]
   if install:
     args += ['--target', 'install']
   do_call(args)
+
+  shutil.rmtree(build_dir)
 
 run_build('Foo', 'Release', install=True, verbose=False)
 run_build('Foo', 'Debug', install=True, verbose=False)
 run_build('Boo', 'Release', install=False, verbose=True)
 run_build('Boo', 'Debug', install=False, verbose=True)
-run_build('Boo', '', install=False, verbose=True)

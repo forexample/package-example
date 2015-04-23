5c0e81c9ff526bdcc6c9bd00a7119a28177ef082
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-23 21:56:59 +0200

    Add options: --shared and --install-boo

--- jenkins.py
+++ jenkins.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
+import argparse
+import glob
 import os
 import shutil
 import subprocess
 import sys
 
+parser = argparse.ArgumentParser(description="Testing script")
+parser.add_argument('--shared', action='store_true', help='Build shared libs')
+parser.add_argument(
+    '--install-boo', action='store_true', help='Install boo and run'
+)
+cmd_args = parser.parse_args()
+
 cwd = os.getcwd()
+exe_dir = os.path.join(cwd, '_install', 'bin')
+
+if not cmd_args.install_boo and cmd_args.shared:
+  # windows and cygwin
+  os.environ['PATH'] = '{};{}'.format(exe_dir, os.environ['PATH'])
 
 def do_call(args):
   oneline = ''
@@ -13,8 +27,6 @@ def do_call(args):
     oneline += ' "{}"'.format(i)
   print('[{}]>{}'.format(os.getcwd(), oneline))
   try:
-    runtime = os.path.join(cwd, '_install', 'bin')
-    os.environ['PATH'] = '{};{}'.format(runtime, os.environ['PATH'])
     subprocess.check_call(args, env=os.environ)
   except subprocess.CalledProcessError as error:
     print(error)
@@ -53,6 +65,11 @@ def run_build(projname, buildtype, install, verbose, test):
     args += ['-DCMAKE_{}_POSTFIX=-{}'.format(buildtype.upper(), buildtype)]
   if verbose:
     args += ['-DCMAKE_VERBOSE_MAKEFILE=ON']
+  if cmd_args.shared:
+    args += ['-DBUILD_SHARED_LIBS=ON']
+  else:
+    args += ['-DBUILD_SHARED_LIBS=OFF']
+
   do_call(args)
 
   args = ['cmake', '--build', build_dir, '--config', buildtype]
@@ -66,9 +83,17 @@ def run_build(projname, buildtype, install, verbose, test):
     do_call(args)
     os.chdir(cwd)
 
-  shutil.rmtree(build_dir)
-
 run_build('Foo', 'Release', install=True, verbose=False, test=False)
 run_build('Foo', 'Debug', install=True, verbose=False, test=False)
-run_build('Boo', 'Release', install=False, verbose=True, test=True)
-run_build('Boo', 'Debug', install=False, verbose=True, test=True)
+
+if cmd_args.install_boo:
+  run_build('Boo', 'Release', install=True, verbose=True, test=False)
+  run_build('Boo', 'Debug', install=True, verbose=True, test=False)
+  executables = glob.glob(os.path.join(exe_dir, 'boo*'))
+  if len(executables) != 2:
+    sys.exit('Expected two executables')
+  for x in executables:
+    do_call([x])
+else:
+  run_build('Boo', 'Release', install=False, verbose=True, test=True)
+  run_build('Boo', 'Debug', install=False, verbose=True, test=True)
